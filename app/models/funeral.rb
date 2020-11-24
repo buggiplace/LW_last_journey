00c129e2cabf861e725e0cc3feafdb138fd636b4
Maximilian Knyphausen <maxiknyphausen@gmail.com>
2020-11-24 17:02:31 +0100

    inserted faker in gemfiles and implemented funeral seed JSON

--- app/models/funeral.rb
+++ app/models/funeral.rb
@@ -1,6 +1,6 @@
 class Funeral < ApplicationRecord
   belongs_to :user
 
-  validates :representative_email, presence: true, format: {with /\A[^@\s]+@[^@\s]+\z/}
-  validates
+  # validates :representative_email, presence: true, format: {with /\A[^@\s]+@[^@\s]+\z/}
+  serialize :preferences
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require_relative 'json'
 
 puts "Starting seed"
 
@@ -15,22 +14,72 @@ Funeral.destroy_all
 
 puts "Adding funerals"
 
-filepath = "db/preferences.json"
+# filepath = "db/preferences.json"
+# serialized_preferences = File.read(filepath) #returns a string
+# preferences = JSON.parse(serialized_preferences)
 
-serialized_preferences = File.read(filepath) #returns a string
-preferences = JSON.parse(serialized_preferences)
+Funeral.create!(
+  preferences: {
+    'funeral_type': {
+      'type': 'Peace forest',
+      'funeral_comment': 'I would like a lot of white candles at the memorial service',
+      'location': 'xxxx'
+    },
+    'music': {
+      'spotify_link': 'https://open.spotify.com/playlist/09vrf9JgCt1AD06mqwlubq?si=Hd9IknaZTQCbIzL6MbxORQ'
+    },
+    'documents': {
+      'Organ donor card': 'https://images.unsplash.com/photo-1564069114553-7215e1ff1890?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=889&q=80',
+      'Last will': 'https://images.unsplash.com/photo-1564069114553-7215e1ff1890?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=889&q=80',
+      'doc_comment': 'Original documents can be found in my office desk at the end of the room.'
+    },
+    'representative': {
+      'first_name': 'Simon',
+      'last_name': 'Geierbach',
+      'email': 'simon.geierbach@gmail.com',
+      'rep_comment': 'Hang in there, buddy! Please make sure my parents dont get to see my nude pictures in whatsapp.'
+    },
+    'digital_will': {
+      'cancel_accounts': ['LinkedIn','Spotify','Facebook','Twitter'],
+      'facebook_obituary': 'No',
+      'bank_accounts': ['Sparkasse ****2456','N26: ****2456', 'Coinbase'],
+      'insurance_accounts': ['HUB24: ****2635','Clark: ****4659'],
+      'hardware': 'Please destroy my harddrives',
+      'dig_comment': 'Yes, you can!'
+    },
+    'obituary': {
+      'last_words': 'Thank you, I had a jolly good time with everyone. On my funeral I would like all of you drink a shot and throw the glasses on the wall! Tschaka!',
+      'spotify_list': 'Yes',
+      'death_date': '',
+      'death_location': '',
+      'funeral_time': '',
+      'funeral_location': '',
+      'funeral_info': '',
+      'obituary_quote': '',
+      'obituary_intro': '',
+      'obituary_relatives': '',
+      'obituary_other': ''
+    },
+  },
+  user_id: User.all.sample.id,
+)
+  # picture_one: row['picture_link'],
+
 
 puts "User Seed done - x added"
 
 puts "Seed finished"
 
 
-
-# 20.times do
-#   Funeral.create!(
-#     email: Faker::Internet.email,
-#     password: "123456",
-#     first_name: Faker::Name.first_name,
-#     last_name: Faker::Name.last_name
-#   )
-# end
+# Funeral.create!(
+#   preferences: {
+#     funeral_type: {
+#       type: 'forrest funeral',
+#       wish_comment: 'free text fiel input'
+#     },
+#     music: {
+#       song_selection: ['ave maria', 'highway to hell'],
+#       wish_comment: 'free text field input'
+#     }
+#   },
+#   representative_email: 'hello@gmail.com'
