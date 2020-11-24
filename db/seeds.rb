fa1077c26a2d1fe51d16e2e2db9bde0d7c785db3
Maximilian Knyphausen <maxiknyphausen@gmail.com>
2020-11-24 13:15:57 +0100

    installed faker and built basic seed file ready to parse in json data and created 2 json files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require_relative 'json'
+
+puts "Starting seed"
+
+puts "Cleaning old seeds"
+
+Funeral.destroy_all
+
+puts "Adding funerals"
+
+filepath = "db/preferences.json"
+
+serialized_preferences = File.read(filepath) #returns a string
+preferences = JSON.parse(serialized_preferences)
+
+puts "User Seed done - x added"
+
+puts "Seed finished"
+
+
+
+# 20.times do
+#   Funeral.create!(
+#     email: Faker::Internet.email,
+#     password: "123456",
+#     first_name: Faker::Name.first_name,
+#     last_name: Faker::Name.last_name
+#   )
+# end
