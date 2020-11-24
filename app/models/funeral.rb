8fbd833d181f629abdc422f7602b1f90320787d4
Jessica Lacroix <jess.dengler@gmail.com>
2020-11-24 23:06:55 +0100

    Email validation for representative

--- app/models/funeral.rb
+++ app/models/funeral.rb
@@ -5,4 +5,6 @@ class Funeral < ApplicationRecord
   serialize :preferences
 
   has_many :condolences, dependent: :destroy
+
+  validates :representative_email, format: { with: /\A[^@\s]+@[^@\s]+\z/ }, allow_blank: true
 end
