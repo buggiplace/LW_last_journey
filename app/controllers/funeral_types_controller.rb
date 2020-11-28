1dcb045bce65ef1660deb60303cc2301b4181259
Maximilian Knyphausen <maxiknyphausen@gmail.com>
2020-11-28 17:48:25 +0100

    added clouds as markers

--- app/controllers/funeral_types_controller.rb
+++ app/controllers/funeral_types_controller.rb
@@ -12,8 +12,8 @@ class FuneralTypesController < ApplicationController
       {
         lat: location.latitude,
         lng: location.longitude,
-        infoWindow: render_to_string(partial: "info_window", locals: { location: location })
-        # image_url: helpers.asset_url('REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS')
+        infoWindow: render_to_string(partial: "info_window", locals: { location: location }),
+        image_url: helpers.asset_url('cloudy_marker_shine.png')
       }
     end
   end
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -13,7 +13,16 @@ const buildMap = (mapElement) => {
 const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
     const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
-    new mapboxgl.Marker()
+
+    // Create a HTML element for your custom marker
+    const element = document.createElement('div');
+    element.className = 'marker';
+    element.style.backgroundImage = `url('${marker.image_url}')`;
+    element.style.backgroundSize = 'contain';
+    element.style.width = '50px';
+    element.style.height = '50px';
+
+    new mapboxgl.Marker(element)
       .setLngLat([ marker.lng, marker.lat ])
       .setPopup(popup)
       .addTo(map);
