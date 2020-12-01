2530249c65d2a82cb3eb6d3ebed241882c0c23c1
Maximilian Knyphausen <maxiknyphausen@gmail.com>
2020-12-01 23:13:36 +0100

    minor changes

--- app/javascript/components/filter_burial_type.js
+++ app/javascript/components/filter_burial_type.js
@@ -1,20 +1,16 @@
 const filterBurialType = () => {
   console.log('Hello World')
 
-// 1. select all labels
+// 1. select all labels [done]
  const labels = document.querySelectorAll("form > fieldset > div > div > label")
 labels.forEach((label) => {
   console.log(label)
-// 2. add event listener to each label
+// 2. add event listener to each label [done]
  label.addEventListener('click', (event) => {
   console.log('click');
-
-// 3. redirect to same url with parameter = selected label
-    function showDetails(params)
-    {
-      window.location = '/funeral_types/12?burial_type='+params;
-    }
-// 4. select radio btn with params
+// 3. redirect to same url with parameter = selected label [ ]
+      window.location.href = window.location.href + "&commit=Filter"
+// 4. select radio btn with params => in html
  })
 });
 
